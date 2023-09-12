05c986318252afa0621ffe33b1032af01d3c52c1
Hugo Morvan <98044151+hugo-morvan@users.noreply.github.com>
2023-09-12 11:21:42 +0200

    updated dijk algo

--- lab3.R
+++ lab3.R
@@ -5,16 +5,10 @@ dijkstra <- function(graph, init.node){
 
     #pseudo code (source = https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
 
-    #1 Mark all nodes unvisited. Create a set of all the unvisited nodes called the unvisited set.
-
-    dist <- c()
+ dist <- c()
     prev <- c()
     Q <- c()
     
-    #2 Assign to every node a tentative distance value: 
-    #  set it to zero for our initial node and to infinity for all other nodes. 
-    # Set the initial node as current.
-
     for (i in 1:nrow(graph)){
         dist[i] <- Inf
         prev[i] <- NA
@@ -23,28 +17,30 @@ dijkstra <- function(graph, init.node){
     dist[init.node] <- 0
     current <- init.node
 
-    #3 For the current node, consider all of its unvisited neighbors and calculate their tentative distances.
-    # Compare the newly calculated tentative distance to the current assigned value and assign the smaller one.
-    # Otherwise the current assigned value will be kept.
-
-
-    #4 When we are done considering all of the unvisited neighbors of the current node,
-    # mark the current node as visited and remove it from the unvisited set.
-    # A visited node will never be checked again.
-
-
-
-    #5 If the destination node has been marked visited (when planning a route between two specific nodes)
-    # or if the smallest tentative distance among the nodes in the unvisited set is infinity (when planning
-    # a complete traversal; occurs when there is no connection between the initial node and remaining unvisited nodes),
-    # then stop. The algorithm has finished.
-
-
-
-    #6 Otherwise, select the unvisited node that is marked with the smallest tentative distance,
-    # set it as the new "current node", and go back to step 3.
-
-
+    while( length(Q) > 0){
+        #find the node in Q with the smallest dist
+        min.dist <- Inf
+        for (i in 1:length(Q)){
+            if (dist[Q[i]] < min.dist){
+                min.dist <- dist[Q[i]]
+                current <- Q[i]
+            }
+        }
+        #remove current from Q
+        Q <- Q[Q != current]
+        #for each neighbor of current
+        for (i in 1:nrow(graph)){
+            if (graph[i,1] == current){
+                neighbor <- graph[i,2]
+                alt <- dist[current] + graph[i,3]
+                if (alt < dist[neighbor]){
+                    dist[neighbor] <- alt
+                    prev[neighbor] <- current
+                }
+            }
+        }
+    }
+return (list(dist=dist, prev=prev))
 }
 
 wiki_graph <-
